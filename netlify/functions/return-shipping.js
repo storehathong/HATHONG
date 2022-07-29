f2dab016ed0d900d5c09c600381178ac285bd20b
a1808891 <gianghi.pham@student.adelaide.edu.au>
2022-07-29 18:22:57 +0930

    fix shipping rate

--- netlify/functions/return-shipping.js
+++ netlify/functions/return-shipping.js
@@ -12,14 +12,8 @@ const handler = async function (event, context) {
     var currency = request.content.currency;
     var country = request.content.shippingAddressCountry;
     var weight = request.content.totalWeight;
-    var province = request.content.shippingAddressProvince;
-    console.log(request.content.customFields[1]);
-    if (request.content.customFields[1].value != "null"){
-        province = request.content.customFields[1].value;
-    };
-    console.log("current province: " + province);
     if (weight == "0"){
-        weight = 500;
+        weight = 1000;
     }
     const response = await axios.get(`https://api.myems.vn/EmsDosmetic?CountryCode=${country}&FromProvince=0&FromDistrict=0&ToProvince=0&ToDistrict=0&weight=${weight}&totalAmount=0&Istype=2&language=0`)
     .then(function (response) {
@@ -32,78 +26,34 @@ const handler = async function (event, context) {
         console.log(error);
     })
     console.log(reponse_output);
-    var noi_thanh = ["Quận 1", "Quận 2", "Quận 3", "Quận 4", "Quận 5", "Quận 6", "Quận 7", "Quận 8", "Quận 10", "Quận 11", "Tân Bình", "Tân Phú", "Phú Nhuận", "Bình Thạnh", "Gò Vấp"];
-    var ngoai_thanh = ["Quận 9", "Quận 12", "Thủ Đức", "Bình Tân", "Hóc Môn", "Bình Chánh", "Nhà Bè", "Củ Chi"];
 
     if (country == "VN"){
-        console.log("country is VN");
-        if (noi_thanh.indexOf(province) > -1 )
-        {
-        console.log("noi thanh");
-        if (currency == "vnd" ){
-            URljson = {
-            rates: [{
-                cost: 26000,
-                description: `Domestic shipping | Giao hàng Nội Địa`
-                }]
-            }
-        } else {
-            URljson = {
-            rates: [{
-                cost: 2,
-                description: `Domestic shipping | Giao hàng Nội Địa`
-                }]
-            }
-        }
-        }
-        else if (ngoai_thanh.indexOf(province) > -1)
-        {
-        console.log("ngoai thanh");
-        if (currency == "vnd" ){
-            URljson = {
+        URljson = {
             rates: [{
                 cost: 30000,
-                description: `Domestic shipping | Giao hàng Nội Địa`
+                description: `Local Shipping`
                 }]
             }
-        } else {
-            URljson = {
-            rates: [{
-                cost: 2,
-                description: `Domestic shipping | Giao hàng Nội Địa`
-                }]
-            }
-        }
-        }
-        else
-        {
-        console.log("cac tinh khac");
-        if (currency == "vnd" ){
-            URljson = {
-            rates: [{
-                cost: 40000,
-                description: `Domestic shipping | Giao hàng Nội Địa`
-                }]
-            }
-        } else {
-            URljson = {
-            rates: [{
-                cost: 2,
-                description: `Domestic shipping | Giao hàng Nội Địa`
-                }]
-            }
-        }
-        }
     }
     else {
         if (currency == "vnd" ){
             URljson = {
                 rates: [{
                     cost: reponse_output.Message[0].Rates,
-                    description: `International shipping | Giao hàng Quốc Tế`
+                    description: `International shipping`
                     }]
                 }
-        } else {
+        } 
+        else if (currency == "cny"){
+            var cny = Math.round((Number(reponse_output.Message[0].Rates))/3500);
+            URljson = {
+                rates: [{
+                    cost: cny,
+                    description: `International shipping`
+                    }]
+                }
+        }
+        else {
             var usd = Math.round((Number(reponse_output.Message[0].Rates))/22000);
             URljson = {
                 rates: [{
